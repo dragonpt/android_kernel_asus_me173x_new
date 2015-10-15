71ee47bd2aad81483e6ac60b42f2053477fb0e99
Sabrina Dubroca <sd@queasysnail.net>
2015-10-15 12:25:00 -0500

    net: add length argument to skb_copy_and_csum_datagram_iovec
    Without this length argument, we can read past the end of the iovec in
    memcpy_toiovec because we have no way of knowing the total length of the
    iovec's buffers.
    
    This is needed for stable kernels where 89c22d8c3b27 ("net: Fix skb
    csum races when peeking") has been backported but that don't have the
    ioviter conversion, which is almost all the stable trees <= 3.18.
    
    This also fixes a kernel crash for NFS servers when the client uses
     -onfsvers=3,proto=udp to mount the export.
    
    Change-Id: I1865e3d7a1faee42a5008a9ad58c4d3323ea4bab
    Signed-off-by: Sabrina Dubroca <sd@queasysnail.net>
    Reviewed-by: Hannes Frederic Sowa <hannes@stressinduktion.org>

--- net/core/datagram.c
+++ net/core/datagram.c
@@ -686,10 +686,13 @@ EXPORT_SYMBOL(__skb_checksum_complete);
  *			   can be modified!
  */
 int skb_copy_and_csum_datagram_iovec(struct sk_buff *skb,
-				     int hlen, struct iovec *iov)
+				     int hlen, struct iovec *iov, int len)
 {
 	__wsum csum;
 	int chunk = skb->len - hlen;
+
+	if (chunk > len)
+		chunk = len;
 
 	if (!chunk)
 		return 0;
