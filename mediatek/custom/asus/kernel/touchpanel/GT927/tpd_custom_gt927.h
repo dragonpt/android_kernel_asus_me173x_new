991f980790544211e807e3df96264c2d08cb7561
Cesar <cesar.maximo@gmail.com>
2019-09-02 23:08:05 +0100

    TP: GT927: tpd_custom: Enable GTP_ESD_PROTECT

--- mediatek/custom/asus/kernel/touchpanel/GT927/tpd_custom_gt927.h
+++ mediatek/custom/asus/kernel/touchpanel/GT927/tpd_custom_gt927.h
@@ -87,7 +87,7 @@ extern int i2c_read_bytes(struct i2c_client *client, u16 addr, u8 *rxbuf, int le
 #define GTP_HAVE_TOUCH_KEY    0
 #define GTP_POWER_CTRL_SLEEP  0       //turn off power on suspend
 #define GTP_CHANGE_X2Y        0				//set for se1
-#define GTP_ESD_PROTECT       0
+#define GTP_ESD_PROTECT       1
 #define GTP_CREATE_WR_NODE    0
 #define GUP_USE_HEADER_FILE   0
 
