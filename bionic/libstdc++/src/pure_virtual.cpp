492d21d049fca4b623c30af705aad477048b5c87
Cesar <cesar.maximo@gmail.com>
2019-11-09 14:52:17 +0000

    cleaning: rm 'bionic' dir and scripts

--- bionic/libstdc++/src/pure_virtual.cpp
+++
@@ -1,8 +0,0 @@
-#undef NDEBUG
-#include <assert.h>
-
-extern "C" void __cxa_pure_virtual() {
-  // We can't call __libc_android_log_write from libstdc++ because it's private, so cheat.
-  assert(!"Pure virtual function called. Are you calling virtual methods from a destructor?");
-  /* NOTREACHED */
-}
